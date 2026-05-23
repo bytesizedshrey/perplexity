bb6464ecc8ade929f0aa1f9b8ff9f93f8aa6ee7c
bytesizedshrey <shreyash18gajbhiye@icloud.com>
2026-05-23 09:37:51 +0530

    auth ddone

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
 export function authUser(req, res, next) {
-  const token = req.cookies.tokens;
+  const token = req.cookies.token;
 
   if (!token) {
     return res.status(400).json({
